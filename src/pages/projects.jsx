b2b3849e0b931e76d3ea6b583d252803afa24ff8
TaylorGooge <taylorgooge@gmail.com>
2023-12-12 14:08:36 +0000

    removed unused imports

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,13 +5,11 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
 
 import Button from "@mui/material/Button";
 
